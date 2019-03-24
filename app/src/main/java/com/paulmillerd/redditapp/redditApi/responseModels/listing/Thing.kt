d48481ad48173ae18704d7078afe4dcafad14932
Paul Millerd <paulmillerd@gmail.com>
2019-03-24 17:40:41 -0400

    Removed Room.

--- app/src/main/java/com/paulmillerd/redditapp/redditApi/responseModels/listing/Thing.kt
+++ app/src/main/java/com/paulmillerd/redditapp/redditApi/responseModels/listing/Thing.kt
@@ -1,21 +1,20 @@
 package com.paulmillerd.redditapp.redditApi.responseModels.listing
 
-import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 import javax.annotation.Generated
 
-@Entity
 @Generated("com.robohorse.robopojogenerator")
 data class Thing(
 
-        @field:SerializedName("data")
+	@field:SerializedName("data")
 	val data: ThingData? = null,
 
-        @field:SerializedName("kind")
+	@field:SerializedName("kind")
 	val kind: String? = null,
 
-		var tempLikes: Boolean? = null,
+	var tempLikes: Boolean? = null,
 
-		var tempScore: Int? = null
+	var tempScore: Int? = null
+
 ): Serializable
