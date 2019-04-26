ee9a5918630e9895e2401a65988e902d78960851
Paul Millerd <paulmillerd@gmail.com>
2019-04-26 19:43:44 -0400

    Code formatting

--- app/src/main/java/com/paulmillerd/redditapp/models/Comment.kt
+++ app/src/main/java/com/paulmillerd/redditapp/models/Comment.kt
@@ -1,3 +1,9 @@
 package com.paulmillerd.redditapp.models
 
-data class Comment(val id: String, val author: String, val score: Int, val body: String, val replies: Listing) : Thing()
+data class Comment(
+        val id: String,
+        val author: String,
+        val score: Int,
+        val body: String,
+        val replies: Listing
+) : Thing()
--- app/src/main/java/com/paulmillerd/redditapp/models/Link.kt
+++ app/src/main/java/com/paulmillerd/redditapp/models/Link.kt
@@ -1,3 +1,12 @@
 package com.paulmillerd.redditapp.models
 
-data class Link(val id: String, val author: String, val title: String, val subreddit: String, val score: Int, val url: String?, val thumbnail: String?, val selfText: String?) : Thing()
+data class Link(
+        val id: String,
+        val author: String,
+        val title: String,
+        val subreddit: String,
+        val score: Int,
+        val url: String?,
+        val thumbnail: String?,
+        val selfText: String?
+) : Thing()
